ab143930c547ba94dc5e563c22e1087e1222ae46
Dusan Rajlic <dusan.rajlic1@gmail.com>
2022-11-24 01:15:24 +0100

    Remove unnecessary css and import

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
-import ClientOnly from "../components/ClientOnly";
 import React from "react";
 import ShipData from "../components/Ships/ShipData";
 
